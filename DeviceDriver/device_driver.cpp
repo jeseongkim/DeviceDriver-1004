ce520a734459e80124a5907ef8cfe24c0b2387d6
Jinchan <ghkdwlscks@gmail.com>
2025-07-03 15:28:20 +0900

    [feature] 5 normal read

--- DeviceDriver/device_driver.cpp
+++ DeviceDriver/device_driver.cpp
@@ -1,13 +1,24 @@
 #include "device_driver.h"
 
+#include <vector>
+using std::vector;
+
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {
 }
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    vector<int> arr;
+    int ret;
+    for (int i = 0; i < 5; i++) {
+        ret = (int)(m_hardware->read(address));
+        arr.push_back(ret);
+    }
+    for (int i = 0; i < 4; i++) {
+        //if(arr[i] != arr[i+1]) exception
+    }
+    return ret;
 }
 
 void DeviceDriver::write(long address, int data)
--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,13 +1,27 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
-TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
+using namespace testing;
+
+class MockFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+
+TEST(DeviceDriver, ReadFromHW_Add0) {
+	MockFlashMemoryDevice mockhardware;
+	EXPECT_CALL(mockhardware, read)
+		.WillRepeatedly(Return(0));
+	FlashMemoryDevice* hardware = &mockhardware;
+
 	DeviceDriver driver{ hardware };
 	int data = driver.read(0xFF);
 	EXPECT_EQ(0, data);
 }
+
+
 
 int main() {
 	::testing::InitGoogleMock();
